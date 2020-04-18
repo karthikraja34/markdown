6f4cbf7d6bb9b3f375f6706e0a20170f40d6799a
Karthik <rajakarthik131@gmail.com>
2020-04-18 14:31:32 +0530

    Add base container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import {markdown} from "markdown";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markdown: "",
+      html: ""
+    }
+  }
 
-export default App;
+  handleChange = (e) => {
+    this.setState({
+      html: markdown.toHTML(e.currentTarget.textContent)
+    })
+    document.getElementById("output").innerHTML = this.state.html
+  }
+
+  convertToHtml() {
+    markdown.toHTML("Hello *World*")
+  }
+
+  render() {
+    return (
+      <div className="container mt-5">
+        <h1 className="text-center"> Type Markdown text </h1>
+
+        <div contentEditable="true" onInput={this.handleChange} className="mt-5 markdown-container">
+          This text can be edited by the user.
+        </div>
+
+        <h1 className="text-center"> Output </h1>
+        <div id="output"> </div>
+      </div>
+    );
+  }
+}
